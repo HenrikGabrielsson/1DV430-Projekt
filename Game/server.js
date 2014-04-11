2acd4ffe0cb481d8b7ff9cb424fdf85094ab3276
OpenShift guest <hg222aa@student.lnu.se>
2014-04-11 11:52:27 -0400

    hello,world...

--- Game/server.js
+++ Game/server.js
@@ -1,15 +1,22 @@
 var http = require("http");
 var fs = require("fs");
+var io = require('socket.io/index')
 
-var httpServer = http.createServer(handler);
-
-function handler(req,response)
+var httpServer = http.createServer(function(req,res)
 {
-    fs.readFile("index.html", function(err,data){
-        response.writeHead(200, {"Content-Type":"text/html"});
-        //response.write("hellloooo");
-        response.end();        
-    })
+    fs.readFile(__dirname + "/index.html",function(err,data)
+    {    res.writeHead(200, {"Content-Type":"text/html"});
+    res.write(data);
+    res.end();})  
 
-}
+})
+
+var serv_io = io.listen(httpServer);
+serv_io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
 httpServer.listen(8080);
